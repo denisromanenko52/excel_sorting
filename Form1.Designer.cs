dda24073e44985006f01d1a1eaa6ebbe21a08728
Денис Романенко <denromanenko52@gmail.com>
2024-03-25 18:38:33 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace excel_sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenXlsx = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonOpenXlsx
+            // 
+            this.buttonOpenXlsx.Location = new System.Drawing.Point(155, 186);
+            this.buttonOpenXlsx.Name = "buttonOpenXlsx";
+            this.buttonOpenXlsx.Size = new System.Drawing.Size(198, 42);
+            this.buttonOpenXlsx.TabIndex = 1;
+            this.buttonOpenXlsx.Text = "Отсортировать таблицу";
+            this.buttonOpenXlsx.UseVisualStyleBackColor = true;
+            this.buttonOpenXlsx.Click += new System.EventHandler(this.buttonOpenXlsx_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "по возрастанию",
+            "по убыванию"});
+            this.comboBox1.Location = new System.Drawing.Point(389, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonOpenXlsx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenXlsx;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace excel_sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedItem = comboBox1.Items[0];
+        }
+        private void buttonOpenXlsx_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Xlsx files (*.xlsx)|*.xlsx";
+            openFileDialog1.FileName = "";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr != DialogResult.No && dr != DialogResult.Cancel && dr != DialogResult.Abort && dr != DialogResult.None)
+            {
+                try
+                {
+                    string filename = openFileDialog1.FileName;
+
+                    ReadCSVData(filename);
+
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("Закройте файл!");
+                }
+            }
+        }
+        
+        public void ReadCSVData(string csvFileName)
+        {
+            string sheetName = "Sheet 1";
+            var csvFilereader = new System.Data.DataTable();
+            csvFilereader = ReadExcel(csvFileName, sheetName);
+
+            if (csvFilereader != null)
+            {
+                GetSortedTable(csvFilereader);
+            }
+        }
+
+        private System.Data.DataTable ReadExcel(string fileName, string sheetName)
+        {
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+            using (var pck = new OfficeOpenXml.ExcelPackage(new FileInfo(fileName)))
+            {
+                var ws = pck.Workbook.Worksheets[0];
+
+                System.Data.DataTable tbl = new System.Data.DataTable();
+
+                if (ws.Dimension != null)
+                {
+                    // Создаем столбцы таблицы на основе названий ячеек в первой строке Excel.
+                    foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                    {
+                        tbl.Columns.Add(firstRowCell.Text);
+                    }
+
+                    try
+                    {
+                        // Добавляем данные в таблицу.
+                        for (int rowNumber = 2; rowNumber <= ws.Dimension.End.Row; rowNumber++)
+                        {
+                            var row = ws.Cells[rowNumber, 1, rowNumber, ws.Dimension.End.Column];
+                            DataRow newRow = tbl.Rows.Add();
+                            foreach (var cell in row)
+                            {
+                                newRow[cell.Start.Column - 1] = cell.Value;
+                            }
+                        }
+
+                        return tbl;
+                    }
+                    catch (System.Data.ConstraintException ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка");
+                        return null;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файл пустой");
+                    return null;
+                }
+            }
+        }
+
+        public void GetSortedTable(DataTable dt)
+        {
+            DataTable sortedDt = new DataTable();
+
+            try
+            {
+                if (comboBox1.SelectedItem.ToString() == "по возрастанию")
+                {
+                    //сортируем таблицу по возрастанию по первому столбцу
+                    sortedDt = dt.AsEnumerable().OrderBy(x => x.Field<object>(dt.Columns[0])).CopyToDataTable();
+                }
+                else if (comboBox1.SelectedItem.ToString() == "по убыванию")
+                {
+                    //сортируем таблицу по убыванию по первому столбцу
+                    sortedDt = dt.AsEnumerable().OrderByDescending(x => x.Field<object>(dt.Columns[0])).CopyToDataTable();
+                }
+
+                //сохраняем в новый документ
+                saveFileDialog1.Filter = "Xlsx files (*.xlsx)|*.xlsx";
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string filename1 = saveFileDialog1.FileName;
+
+                        using (var package = new ExcelPackage())
+                        {
+                            var worksheet = package.Workbook.Worksheets.Add("SortedData");
+                            worksheet.Cells["A1"].LoadFromDataTable(sortedDt, true);
+                            FileInfo fi = new FileInfo(filename1);
+                            package.SaveAs(fi);
+                            package.Dispose();
+                        }
+                    }
+                    catch (System.InvalidOperationException)
+                    {
+                        MessageBox.Show("Закройте файл!");
+                    }
+                }
+            }
+            catch (System.InvalidOperationException)
+            {
+                MessageBox.Show("Источник не содержит строк");
+            }
+        }
+    }
+}
